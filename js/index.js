8e545f623b2bc80e854b96482053b9d1cc83b783
CristianPoli <136546424+CristianPoli@users.noreply.github.com>
2024-10-05 21:23:51 -0500

    Update index.js "Se adiciona menú del Loguin"

--- js/index.js
+++ js/index.js
@@ -1,3 +1,30 @@
 function IngresarDashboard() {
     location.href = 'dashboard.html';
 }
+
+function IngresarRecord() {
+    location.href = 'Record.html';
+}
+
+function IngresarRecoverPassword() {
+    location.href = 'RecoverPassword.html';
+}
+
+function Suscribirse() {
+    var successMessage = document.getElementById("successMessage");
+    successMessage.style.display = "block"; 
+    setTimeout(function() {
+      window.location.href = "index.html"; 
+    }, 4000); 
+    return false; 
+  }
+
+  function NotificationRecoverPassword() {
+   
+    alert("Se notifica los pasos para recuperacion de la cuenta registrada al correo");
+
+    setTimeout(function() {
+        window.location.href = "index.html"; 
+      }, 2000); 
+      return false; 
+}
